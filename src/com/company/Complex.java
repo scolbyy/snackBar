9ac3238be4d8e0d2c404677606d24241663b2c77
Shawn <shawncolby@billericak12.com>
2020-02-07 12:28:09 -0500

    fdhh

--- src/com/company/Complex.java
+++ src/com/company/Complex.java
@@ -21,6 +21,21 @@ public class Complex
     }
     public Complex add (Complex other)
     {
-
+        double addFirst = first + other.first;
+        double addSecond = second + other.second;
+        return new Complex(addFirst,addSecond);
+    }
+    public String toString ()
+    {
+        String answer = first + " + " + second + "i";
+        return answer;
+    }
+    public static void main(String[] args)
+    {
+        Complex a = new Complex(6.9, 2.42);
+        System.out.println (a.abs());
+        Complex b = new Complex(7.3, 8.889);
+        System.out.println(b.add(a));
+        System.out.println(a);
     }
 }
